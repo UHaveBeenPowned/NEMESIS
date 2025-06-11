4c6823dd7b8dc3521972e454c1309dcd3b56a060
π <207250801+UHaveBeenPowned@users.noreply.github.com>
2025-06-11 02:01:27 +0200

    Deleted rotatingFileHandler. See #1
    Not trully needed.

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,17 +1,10 @@
 import logging
-from logging.handlers import RotatingFileHandler
 import os
 from pathlib import Path
 
 LOG_FILE: str = "NEMESIS.log";
 NEMESIS_LOG_FOLDER = Path(os.environ["USERPROFILE"]) / "Desktop" / "NEMESIS"
 NEMESIS_LOG_FOLDER.mkdir(parents=True, exist_ok=True)
-
-handler = RotatingFileHandler(
-    NEMESIS_LOG_FOLDER/LOG_FILE, 
-    maxBytes=50 * 1024 * 1024,  # 10 MB
-    backupCount=5
-)
 
 logging.basicConfig(
     filename=NEMESIS_LOG_FOLDER/LOG_FILE,
